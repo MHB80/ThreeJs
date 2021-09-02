8b24f09b4f6e077cd5e99d5ba11423bb5d969fbb
Fatemeh Shoushtarizadeh <89273670+FShoushtari@users.noreply.github.com>
2021-09-02 20:02:45 +0430

    creat functions

--- src/main.js
+++ src/main.js
@@ -4,14 +4,7 @@ import { OrbitControls } from "three/examples/jsm/controls/orbitcontrols";
 
 import "./style.css";
 
-
 let camera, scene, renderer;
-let plane;
-let pointer, raycaster;
-
-let rollOverMesh, rollOverMaterial;
-let outLineMesh, outRingMesh;
-let obj, controls;
 const objects = [];
 
 init();
@@ -29,43 +22,20 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xf0f0f0);
 
-  // creat circle pointer
-  const rollOverGeo = new THREE.CircleGeometry(50, 50);
-  rollOverMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, opacity: 0.3, transparent: true });
-  rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
-  rollOverGeo.rotateX(- Math.PI / 2);
+  const pointer = creatPointer(50, 5, 0xffffff, 0xffffff, 0.3, 0.7);
+  scene.add(pointer);
 
-  // creat border for pointer
-  const outLineGeo = new THREE.RingGeometry(55, 50, 50);
-  const outLineMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.BackSide, opacity: 0.7, transparent: true });
-  outLineMesh = new THREE.Mesh(outLineGeo, outLineMaterial);
-  outLineGeo.rotateX(- Math.PI / 2);
-
-  // creat a ring that shown when mousedown
-  const outRingGeo = new THREE.RingGeometry(55, 50, 50);
-  const outRingMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.BackSide, transparent: true, visible: false });
-  outRingMesh = new THREE.Mesh(outRingGeo, outRingMaterial);
-  outRingGeo.rotateX(- Math.PI / 2);
-
-  // creat an object an add to scene
-  obj = new THREE.Object3D();
-  obj.add(rollOverMesh);
-  obj.add(outLineMesh);
-  obj.add(outRingMesh);
-  obj.position.y = 5;
-  scene.add(obj);
-
-  controls = new OrbitControls(camera, canvas);
+  const controls = new OrbitControls(camera, canvas);
   controls.target.set(0, 5, 0);
   controls.update();
 
-  raycaster = new THREE.Raycaster();
+  const raycaster = new THREE.Raycaster();
 
-  pointer = new THREE.Vector2();
+  const pointerPosition = new THREE.Vector2();
 
   const geometry = new THREE.PlaneGeometry(1000, 1000);
   geometry.rotateX(- Math.PI / 2);
-  plane = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: "pink" }));
+  const plane = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: "pink" }));
   scene.add(plane);
 
   // add objects that pointer appears on 
@@ -79,35 +49,37 @@ function init() {
   document.addEventListener('mousedown', onMouseDown);
   window.addEventListener('resize', onWindowResize);
 
-  render();
-}
+  function onPointerMove(event) {
 
-function onMouseDown(event) {
-
-  pointer.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
-
-  raycaster.setFromCamera(pointer, camera);
-
-  const intersects = raycaster.intersectObjects(objects);
-
-  if (intersects.length > 0) {
-
-    const intersect = intersects[0];
-
-    new TWEEN.Tween(pointer).to(pointer, 500)
-      .easing(TWEEN.Easing.Cubic.Out)
-      .onUpdate(function () {
-        outRingMesh.material.visible = true;
-        outRingMesh.scale.multiplyScalar(1.02);
-        outRingMesh.material.opacity -= 0.04;
-      })
-      .start()
-      .onComplete(function () {
-        outRingMesh.material.visible = false;
-        outRingMesh.material.opacity = 1;
-        outRingMesh.scale.set(1, 1, 1);
-      });
+    pointerPosition.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
+  
+    raycaster.setFromCamera(pointerPosition, camera);
+  
+    const intersects = raycaster.intersectObjects(objects);
+  
+    if (intersects.length > 0) {
+  
+      const intersect = intersects[0];
+  
+      pointer.position.copy(intersect.point).add(intersect.face.normal);
+      pointer.position.divideScalar(5).floor().multiplyScalar(5).addScalar(3);
+    }
   }
+
+  function onMouseDown(event) {
+
+    pointerPosition.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
+  
+    raycaster.setFromCamera(pointerPosition, camera);
+  
+    const intersects = raycaster.intersectObjects(objects);
+  
+    if (intersects.length > 0) {
+  
+      poniterAnimation(pointer, 1, 2, 0, 1, 500);
+    }
+  }
+
 }
 
 function onWindowResize() {
@@ -118,37 +90,51 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function onPointerMove(event) {
-
-  pointer.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
-
-  raycaster.setFromCamera(pointer, camera);
-
-  const intersects = raycaster.intersectObjects(objects);
-
-  if (intersects.length > 0) {
-
-    const intersect = intersects[0];
-
-    obj.position.copy(intersect.point).add(intersect.face.normal);
-    obj.position.divideScalar(10).floor().multiplyScalar(10).addScalar(15);
-
-  }
-
-  render();
-}
-
 function render() {
 
   renderer.render(scene, camera);
 }
 
+function creatPointer(radius, boderWidth, backgroundColor, borderColor, backgroundOpacity, borderOpacity) {
+  // creat circle pointer
+  const rollOverGeo = new THREE.CircleGeometry(radius, radius);
+  const rollOverMaterial = new THREE.MeshBasicMaterial({ color: backgroundColor, opacity: backgroundOpacity, transparent: true });
+  const rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
+  rollOverGeo.rotateX(- Math.PI / 2);
 
+  // creat border for pointer
+  const outLineGeo = new THREE.RingGeometry((radius + boderWidth), radius, radius);
+  const outLineMaterial = new THREE.MeshBasicMaterial({ color: borderColor, side: THREE.BackSide, opacity: borderOpacity, transparent: true });
+  const outLineMesh = new THREE.Mesh(outLineGeo, outLineMaterial);
+  outLineGeo.rotateX(- Math.PI / 2);
+
+  // creat a ring that shown when mousedown
+  const outRingGeo = new THREE.RingGeometry((radius + boderWidth), radius, radius );
+  const outRingMaterial = new THREE.MeshBasicMaterial({ color: borderColor, side: THREE.BackSide, transparent: true });
+  const outRingMesh = new THREE.Mesh(outRingGeo, outRingMaterial);
+  outRingGeo.rotateX(- Math.PI / 2);
+
+  const obj = new THREE.Object3D();
+  obj.add(rollOverMesh);
+  obj.add(outLineMesh);
+  obj.add(outRingMesh);
+  obj.position.y = 5;
+  return obj;
+}
+
+function poniterAnimation(pointer, minScale, maxScale, minOpacity, maxOpacity, duration) {
+    
+  new TWEEN.Tween({ op: maxOpacity, sc: minScale }).to({ op: minOpacity, sc: maxScale }, duration)
+    .easing(TWEEN.Easing.Cubic.Out)
+    .onUpdate(function (currentState) {
+      pointer.children[2].scale.set(currentState.sc, currentState.sc, currentState.sc);
+      pointer.children[2].material.opacity = currentState.op;
+    })
+    .start()
+}
 
 function animate() {
   requestAnimationFrame(animate)
-
-  controls.update()
 
   TWEEN.update()
 
